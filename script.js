b12c68d40d2ca3a5e859aad5ce201592aeccc647
areeshatehreem <50449369+areeshatehreem@users.noreply.github.com>
2019-07-13 16:06:22 +0500

    wip

---
+++ script.js
@@ -0,0 +1,164 @@
+let resturantManagement = [
+    "decoration",
+    "sittingArramgment",
+    "rooms",
+    "familyHall",
+    "table",
+    "glass",
+    "plates",
+    "fokes",
+    "spoons",
+    "ketchepBottle",
+    "sauses",
+    "pans",
+    "drinks",
+    "tissue",
+    "straws",
+    "knife",
+    "dishSpoons",
+    "ovens"
+] 
+
+let resturantWorkers = [ {
+    name : "sajid",
+    position: "cheif",
+    phoneNumber: 0321546721,
+    email: "sajidKhan@gmail.com",
+    age: 35,
+    address: "nearAirport",
+    qualification: "intermediate",
+
+},
+
+
+{
+    name : "zafer",
+    position: "cheif",
+    phoneNumber: 032455461,
+    email: "zaferAlam@gmail.com",
+    age: 40,
+    address: "near saudabad",
+    qualification: "none",
+    skill: "cheifCourse",
+    
+
+    
+},
+
+
+{
+    name : "jaffer",
+    position: "cheif",
+    phoneNumber: 0210045541,
+    email: "jafferRiaz123@gmail.com",
+    age: 26,
+    address: "near quaidabad",
+    qualification: "matric",
+    skill: "cheifCourse",
+
+    
+}]
+
+
+let resturantWaiter = [{
+    name : "ali",
+    position: "waiter",
+    phoneNumber: 021006731,
+    email: "aliShazeb45@gmail.com",
+    age: 20,
+    address: "near quaidabad",
+    qualification: "none",
+    
+
+},
+
+
+{
+    name : "junaid",
+    position: "waiter",
+    phoneNumber: 021001241,
+    email: "junaidKhan@gmail.com",
+    age: 31,
+    address: "near lasipara",
+    qualification: "matric",
+    
+
+},
+
+
+{
+    name : "moiz",
+    position: "waiter",
+    phoneNumber: 0133345541,
+    email: "moizSaleem123@gmail.com",
+    age: 26,
+    address: "near abbasiMarket",
+    qualification: "Bcom",
+    isJob: "partTime",
+
+}]
+
+
+
+let resturantOwner =  [{
+    name: "muhmmadMirzaSahah",
+    martialStatus: " married",
+    age: 59,
+    qualification : "ACCA " ,
+    phoneNumber: 033215467,
+    email: "MMS.Shahzeb@gmail.com",
+    address: "DHA",
+    religion: "islam",
+    position: "owner",
+
+
+
+},
+
+
+{
+    
+    name: "fahadNaeem",
+    martialStatus: " married",
+    age: 63,
+    qualification : "BBA",
+    phoneNumber: 0232321467,
+    email: "fahadNAeem78@gmail.com",
+    address: "clifton",
+    religion: "islam",
+    position: "CEO",
+
+
+},
+
+
+{
+    name: "shameerJahan",
+    martialStatus: " married",
+    age: 45,
+    qualification : "BBA",
+    phoneNumber: 0232321467,
+    email: "shameerJahan@gmail.com",
+    address: "gulshan",
+    religion: "islam",
+    position: "manager",
+
+},
+
+
+
+{
+    name: "omerFahad",
+    martialStatus: " married",
+    age: 40,
+    qualification : "CA inComplete",
+    phoneNumber: 0232322367,
+    email: "omerFAhad@gmail.com",
+    address: "gulshan e johar",
+    religion: "islam",
+    position: "Accountant",
+    skill :" fastTyping",
+    experience: "four years",
+
+    
+}]
